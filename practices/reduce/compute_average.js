377f489770a0a2083109353438e4bbd429d446a6
awan <awansama@126.com>
2017-08-07 15:18:57 +0000

    reduce 4

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -2,6 +2,12 @@
 
 function compute_average(collection) {
   //在这里写入代码
+  let result ;
+  let sum  = 0
+  for (let i = 0 ; i < collection.length ; i++){
+      sum = sum + collection[i];
+  }
+  return result = sum / collection.length ;
 }
 
 module.exports = compute_average;
